ef284cc8b92eeefbcc8445c8f20fa5ba00c0e241
antsimch <anthonysimch@gmail.com>
2023-09-15 03:30:25 +0800

    docker deployment

--- mtg_server/src/main/java/sg/edu/nus/iss/mtg_server/config/SecurityConfig.java
+++ mtg_server/src/main/java/sg/edu/nus/iss/mtg_server/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests((requests) -> requests
+                .requestMatchers(HttpMethod.GET, "**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/login", "/api/register").permitAll()
                 .anyRequest().authenticated());
         return http.build();
